7668412cdfa2c266b1b2998283db7c489a84b289
Isa <114004297+IsaELuna@users.noreply.github.com>
2023-06-30 16:26:29 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,58 @@
+x = 0;
+y = 0;
+
+drawApple = "";
+
+var SpeechRecognition = window.webkitSpeechRecognition;
+  
+var recognition = new SpeechRecognition();
+
+function start()
+{
+  document.getElementById("status").innerHTML = "O sistema está ouvindo. Por favor, fale.";  
+  recognition.start();
+} 
+ 
+recognition.onresult = function(event) {
+
+ console.log(event); 
+
+ content = event.results[0][0].transcript;
+
+    document.getElementById("status").innerHTML = "A fala foi reconhecida: " + content; 
+
+    to_number = Number(content);
+    if(Number.isInterger(to_number));
+}
+
+function setup() {
+ screen_width = window.innerWidth;
+}
+
+function draw() {
+  if(drawApple == "set")
+  {for(var i = 1; i <= to_number; i++)
+    {
+      x = Math.floor(Math.random() * 700);
+      y = Math.floor(Math.random() * 400);
+      Image(apple, x, y, 50, 50);
+    }
+    document.getElementById("status").innerHTML = toNumber + " maçãs desenhadas";
+    drawApple = "";
+    speak()
+  }
+}
+
+function speak(){
+    var synth = window.speechSynthesis;
+
+    var utterThis = new SpeechSynthesisUtterance(speakData);
+
+    synth.speak(utterThis);
+
+    speakData = "";
+}
+
+function preload() {
+
+}
